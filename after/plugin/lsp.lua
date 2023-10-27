27f9e364e891fb5ee74bd3c676399441b80f20fc
Kyriakos Sidiropoulos <kiriakos.sdr@gmail.com>
2023-10-27 10:41:35 +0300

    change buffer keymaps

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,29 +1,22 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  local opts = {buffer = bufnr, remap = false}
+  local opts = { buffer = bufnr, remap = false }
 
-  lsp_zero.format_on_save({
-    format_opts = {
-      async = false,
-      timeout_ms = 10000,
-    },
-    servers = {
-      ['tsserver'] = {'javascript', 'typescript', 'tsx'},
-      ['lua_ls'] = {'lua'},
-      ['html'] = {'html'},
-      ['clangd'] = {'cpp'},
-      ['cssls'] = {'css'},
-      ['jsonls'] = {'json'}
-    }
-  })
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {"pyright", "clangd", "eslint", "tsserver", "lua_ls", "html", "cssls", "tailwindcss", "jsonls", "dockerls", "docker_compose_language_service"},
+  ensure_installed = { "pyright", "clangd", "eslint", "tsserver", "lua_ls", "html", "cssls", "tailwindcss", "jsonls",
+    "dockerls", "docker_compose_language_service" },
   automatic_installation = true,
   handlers = {
     lsp_zero.default_setup
   }
 })
+
