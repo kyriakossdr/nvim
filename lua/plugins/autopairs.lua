415399d34394e47258ba3bd8ba90f955cf9c1a5b
kyriakossdr <kiriakos.sdr@gmail.com>
2025-05-24 21:13:14 +0300

    add nvim-ts-autotag plugin

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,9 +1,17 @@
 return {
-  "windwp/nvim-autopairs",
-  config = function()
-    require("nvim-autopairs").setup({
-      disable_filetype = { "TelescopePrompt", "vim" },
-      map_cr = true
-    })
-  end
+  {
+    "windwp/nvim-ts-autotag",
+    config = function ()
+      require("nvim-ts-autotag").setup({})
+    end
+  },
+  {
+    "windwp/nvim-autopairs",
+    config = function()
+      require("nvim-autopairs").setup({
+        disable_filetype = { "TelescopePrompt", "vim" },
+        map_cr = true
+      })
+    end
+  },
 }
