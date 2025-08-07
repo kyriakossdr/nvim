547cb6aff62b7bda521af3746eff90b039f99796
kyriakossdr <kiriakos.sdr@gmail.com>
2025-08-07 20:23:34 +0300

    Add nvim-surround

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -10,8 +10,24 @@ return {
     config = function()
       require("nvim-autopairs").setup({
         disable_filetype = { "TelescopePrompt", "vim" },
-        map_cr = true
+        map_cr = true,
+        check_ts = true, -- use treesitter to check for valid pairs
+        ts_config = {
+          lua = {'string'},
+          javascript = {'template_string'},
+          java = false,
+        }
       })
     end
   },
+  {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  }
 }
