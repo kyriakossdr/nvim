bb8ec6e7e93f4bc358f12d81707bb83c751c3d3c
kyriakossdr <kiriakos.sdr@gmail.com>
2025-11-09 20:24:41 +0200

    Update theme

--- lua/plugins/scheme.lua
+++ lua/plugins/scheme.lua
@@ -60,9 +60,61 @@ return {
         hl.CursorLine = { bg = "none" }
         hl.CursorColumn = { bg = "none" }
         hl.VertSplit = { fg = "#444444", bg = "none" }
+        
+        -- Oh-lucy inspired syntax highlighting
+        hl.Comment = { fg = "#6e6b69", italic = true }
+        hl.Constant = { fg = "#e06c75" }
+        hl.String = { fg = "#d19a66" }
+        hl.Character = { fg = "#d19a66" }
+        hl.Number = { fg = "#d19a66" }
+        hl.Boolean = { fg = "#d19a66" }
+        hl.Float = { fg = "#d19a66" }
+        hl.Identifier = { fg = "#dcdcdc" }
+        hl.Function = { fg = "#61afef" }
+        hl.Statement = { fg = "#c678dd" }
+        hl.Conditional = { fg = "#c678dd" }
+        hl.Repeat = { fg = "#c678dd" }
+        hl.Label = { fg = "#c678dd" }
+        hl.Operator = { fg = "#56b6c2" }
+        hl.Keyword = { fg = "#c678dd" }
+        hl.Exception = { fg = "#c678dd" }
+        hl.PreProc = { fg = "#e06c75" }
+        hl.Include = { fg = "#e06c75" }
+        hl.Define = { fg = "#c678dd" }
+        hl.Macro = { fg = "#c678dd" }
+        hl.PreCondit = { fg = "#c678dd" }
+        hl.Type = { fg = "#e5c07b" }
+        hl.StorageClass = { fg = "#e5c07b" }
+        hl.Structure = { fg = "#e5c07b" }
+        hl.Typedef = { fg = "#e5c07b" }
+        hl.Special = { fg = "#56b6c2" }
+        hl.SpecialChar = { fg = "#d19a66" }
+        hl.Tag = { fg = "#e06c75" }
+        hl.Delimiter = { fg = "#dcdcdc" }
+        hl.SpecialComment = { fg = "#6e6b69" }
+        hl.Debug = { fg = "#e06c75" }
       end,
     })
     vim.cmd([[colorscheme tokyonight]])
+    
+    -- Set terminal colors to match monochrome palette
+    vim.g.terminal_color_0 = "#1a1a1a"
+    vim.g.terminal_color_1 = "#444444"
+    vim.g.terminal_color_2 = "#606060"
+    vim.g.terminal_color_3 = "#707070"
+    vim.g.terminal_color_4 = "#888888"
+    vim.g.terminal_color_5 = "#a0a0a0"
+    vim.g.terminal_color_6 = "#bfbfbf"
+    vim.g.terminal_color_7 = "#dcdcdc"
+    vim.g.terminal_color_8 = "#2a2a2a"
+    vim.g.terminal_color_9 = "#4d4d4d"
+    vim.g.terminal_color_10 = "#666666"
+    vim.g.terminal_color_11 = "#777777"
+    vim.g.terminal_color_12 = "#999999"
+    vim.g.terminal_color_13 = "#b0b0b0"
+    vim.g.terminal_color_14 = "#cccccc"
+    vim.g.terminal_color_15 = "#ffffff"
   end
 }
+
 
