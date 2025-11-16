3cccdf49ba0a3bc29ff3aaaa58dc5267338c4a4a
kyriakossdr <kiriakos.sdr@gmail.com>
2025-11-16 17:21:06 +0200

    Show hidden files in neo-tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -17,6 +17,7 @@ return {
       filesystem = {
         follow_current_file = { enabled = true },
         filtered_items = {
+          visible = true,
           hide_dotfiles = false,
           hide_gitignored = true,
         },
