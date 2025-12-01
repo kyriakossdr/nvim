99bfebfb172666aec557a129a189c97e63aae527
kiriakos.sdr@gmail.com <kiriakos.sdr@gmail.com>
2025-12-01 22:08:39 +0200

    Theme updates

--- lua/plugins/scheme.lua
+++ lua/plugins/scheme.lua
@@ -14,35 +14,36 @@ return {
           telescope = true,
           treesitter = true,
         },
-        options = {
-          transparency = true,
-          lualine_transparency = true,
-        }
+        --options = {
+        --  transparency = true,
+        --  lualine_transparency = true,
+        --}
       })
 
       vim.cmd("colorscheme onedark_dark")
     end
   },
-  --{
-  --  "Yazeed1s/oh-lucy.nvim",
-  --  priority = 1000,
-  --  config = function ()
-  --    require("oh-lucy").setup({
-  --      plugins = {
-  --        neogit = true,
-  --        gitsigns = true,
-  --        nvim_cmp = true,
-  --        nvim_lsp = true,
-  --        nvim_notify = true,
-  --        lsp_saga = true,
-  --        telescope = true,
-  --        treesitter = true,
-  --      },
-  --      options = {
-  --        transparency = true,
-  --        lualine_transparency = true,
-  --      }
-  --  })
-  --  end
-  --}
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function ()
+      require("onedarkpro").setup({
+        plugins = {
+          gitsigns = true,
+          nvim_cmp = true,
+          nvim_lsp = true,
+          nvim_notify = true,
+          neo_tree = true,
+          lsp_saga = true,
+          telescope = true,
+          treesitter = true,
+        },
+        --options = {
+        --  transparency = true,
+        --  lualine_transparency = true,
+        --}
+      })
+    end
+  },
 }
